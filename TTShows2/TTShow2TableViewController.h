f3936f490bae276be2303941b2aaf37d7a8a6814
rid0n3 <zulmzmz@MockingBird.local>
2014-10-18 14:41:46 +0800

    commit

---
+++ TTShows2/TTShow2TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTShow2TableViewController.h
+//  TTShows2
+//
+//  Created by ME-Tech Mac User 1 on 10/18/14.
+//  Copyright (c) 2014 ME-Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTShow2TableViewController : UITableViewController
+
+@end
